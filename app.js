f1a1b0499dfdb2a9e59b3497537db8672be8f451
shyr1punk <shyr1punk@gmail.com>
2014-11-05 17:14:00 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('schedule', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('schedule').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('schedule').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
