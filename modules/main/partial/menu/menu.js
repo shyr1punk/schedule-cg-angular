78ea1d356c5c5b0d9864284feda25aeb377cc0bf
shyr1punk <shyr1punk@gmail.com>
2014-11-07 17:31:53 +0300

    Add getGroups service, add menu back button

--- modules/main/partial/menu/menu.js
+++ modules/main/partial/menu/menu.js
@@ -2,27 +2,79 @@ angular.module('main').controller('MenuCtrl',function($scope, menuService, $anim
 
     $animate.enabled(false);
 
+    /**
+     * Save selected menu items
+     *
+     * @type {Object}
+     */
+    $scope.history = {};
+
+    /**
+     * Get faculty list
+     */
     $scope.getFaculties = function () {
         menuService.getFaculties().
             success(function (data) {
                 $scope.buttons = data.faculties;
+                $scope.history.faculty = null;
+                $scope.menuButtonClick = $scope.getSpecialities;
             }).
             error(function (data) {
                 console.log(data);
             });
     };
 
+    /**
+     * Get specialities by faculty
+     *
+     * @param {Number} faculty faculty id
+     */
     $scope.getSpecialities = function (faculty) {
         menuService.getSpecialities(faculty).
             success(function (data) {
                 $scope.buttons = data.spec;
+                $scope.history.faculty = faculty;
+                $scope.history.speciality = null;
+                $scope.menuButtonClick = $scope.getGroups;
             }).
             error(function (data) {
                 console.log(data);
             });
     };
 
-    $scope.menuButtonClick = $scope.getSpecialities;
+    /**
+     * Get groups list by speciality
+     *
+     * @param {Number} speciality speciality id
+     */
+    $scope.getGroups = function (speciality) {
+        menuService.getGroups($scope.history.faculty, speciality).
+            success(function (data) {
+                $scope.buttons = data.groups;
+                $scope.history.speciality = speciality;
+                $scope.menuButtonClick = function () {};
+            }).
+            error(function (data) {
+                console.log(data);
+            });
+    };
+
+    /**
+     * Button back menu
+     *
+     * @param {Number} faculty faculty id
+     * @param {Number} speciality speciality id
+     */
+    $scope.menuBack = function (faculty, speciality) {
+        if (faculty) {
+            if (speciality) {
+                $scope.getSpecialities(faculty, speciality);
+            } else {
+                $scope.getFaculties(faculty);
+            }
+        }
+    };
+
     $scope.getFaculties();
 
 });
--- service/menuService.js
+++ service/menuService.js
@@ -7,7 +7,9 @@ angular.module('schedule').factory('menuService',function($http, constantsServic
          * @returns {HttpPromise}
          */
         getFaculties: function () {
-            return $http.get(constantsService.apiGetEndpoint() + 'faculties');
+            return $http.get(constantsService.apiGetEndpoint() + 'faculties', {
+                //cache: true
+            });
         },
 
         /**
@@ -17,7 +19,22 @@ angular.module('schedule').factory('menuService',function($http, constantsServic
          * @returns {HttpPromise}
          */
         getSpecialities: function (faculty) {
-            return $http.get(constantsService.apiGetEndpoint() + 'faculty/' + faculty);
+            return $http.get(constantsService.apiGetEndpoint() + 'faculty/' + faculty, {
+                //cache: true
+            });
+        },
+
+        /**
+         * Get speciality list by faculty
+         *
+         * @param {Number} faculty faculty id
+         * @param {Number} speciality speciality id
+         * @returns {HttpPromise}
+         */
+        getGroups: function (faculty, speciality) {
+            return $http.get(constantsService.apiGetEndpoint() + 'faculty/f' + faculty + '/speciality/' + speciality, {
+                //cache: true
+            });
         }
     };
 
